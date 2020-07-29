310727c2f82451e7b3534e74fa6f0dce8bf92d86
Tonelyr <40433952+Tonelyr@users.noreply.github.com>
2020-07-28 20:22:07 -0400

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -36,3 +36,25 @@ if input == "Y" or "y":
     player = False
 else:
     print("Bip boup, shutting down...")
+while player == False:
+    player = input("Rock, Paper, Scissors ? ")
+    if player == comp:
+        print("Tie !")
+    elif player == "Rock":
+        if comp == "Paper":
+            print("You lose !", comp, "covers", player)
+        else:
+            print("You win !", player, "covers", comp)
+    elif player == "Paper":
+        if comp == "Scissors":
+            print("You lose !", comp, "cut", player)
+        else:
+            print("You win !", player, "covers", comp)
+    elif player == "Scissors":
+        if comp == "Rock":
+            print("You lose !", comp, "smashes", player)
+        else:
+            print("You win !", player, "cut", comp)
+    else:
+        print("Incorrect Value")
+print("Bip boup, shutting down....")
